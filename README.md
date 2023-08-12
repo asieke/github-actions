1367e86faca090764e0d7ee4b86dd07d8016652b
Alex Sieke <asieke@gmail.com>
2023-08-12 06:31:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Github Actions
+
+Posting solutions to various LEETCode problems to test out various Github Actions
---
+++ leet/1TwoSum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let ans = []; // to store the indices of the two numbers
+  for (let i = 0; i < nums.length; i++) {
+    // loop through each element in the array
+    for (let j = i + 1; j < nums.length; j++) {
+      // loop through the rest of the array to find the other number
+      let sum = nums[i] + nums[j]; // calculate the sum of the two numbers
+      if (sum === target) {
+        // check if the sum equals the target value
+        ans.push(i); // store the indices of the two numbers
+        ans.push(j);
+        return ans; // return the answer
+      }
+    }
+  }
+  return ans; // if no such numbers exist, return an empty vector
+};
