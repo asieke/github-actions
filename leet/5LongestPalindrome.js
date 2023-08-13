01424872c8aced6880bb4b6910ae98d426c5c9d0
Alex Sieke <asieke@gmail.com>
2023-08-13 15:47:38 -0400

    finished problem 5

---
+++ leet/5LongestPalindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (isPalindrome(s)) return s;
+
+  for (let i = s.length - 1; i >= 1; i--) {
+    for (let j = 0; j < s.length - i + 1; j++) {
+      if (isPalindrome(s.substr(j, i))) {
+        return s.substr(j, i);
+      }
+    }
+  }
+};
+
+let isPalindrome = function (s) {
+  for (let i = 0, j = s.length - 1; j >= i; i++, j--) {
+    if (s[i] !== s[j]) return false;
+  }
+  return true;
+};
